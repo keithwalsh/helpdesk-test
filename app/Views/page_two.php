8924945af1df89634a5f9962845733f37cdeeab9
keithwalsh <keithwalsh@gmail.com>
2025-03-07 11:25:23 +0000

    fix(page_two): php variables for dynamic content

--- app/Views/page_two.php
+++ app/Views/page_two.php
@@ -11,11 +11,13 @@ Blog Posts
 <h1 class="text-xl font-bold italic mb-8">All blog posts</h1>
 
 <div class="grid gap-4 grid-cols-2 grid-rows-1">
+<?php foreach ($posts as $post): ?>
     <div class="p-4 border-2">
-        <h2>The Title</h2>
-        <img src="https://picsum.photos/800/300" alt="An Image">
-        <p>The excerpt</p>
+        <h2><?php echo $post['title']; ?></h2>
+        <img src="<?php echo $post['image']; ?>" alt="An Image">
+        <p><?php echo $post['excerpt']; ?></p>
     </div>
+    <?php endforeach; ?>
 </div>
 
 <?= $this->endSection() ?>
