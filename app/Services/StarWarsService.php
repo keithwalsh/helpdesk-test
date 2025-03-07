975fafd37ffd07abc55b713cf39a6843d3163795
keithwalsh <keithwalsh@gmail.com>
2025-03-07 11:28:08 +0000

    fix(page_three): disable ssl verification for dev environment

--- app/Services/StarWarsService.php
+++ app/Services/StarWarsService.php
@@ -15,6 +15,8 @@ class StarWarsService
         $ch = curl_init($url);
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if (ENVIRONMENT === 'development')
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
         curl_close($ch);
 
